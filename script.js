e4b21e884c54b67e5465060726e6e8feb6ea5539
katj2950 <katj2950@stud.kea.dk>
2019-12-02 11:03:11 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let side;
+const url = "https://katjsoeg.dk/kea/2_sem_eksamen/designwp/wordpress/wp-json/wp/v2/pages/";
+
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+	hentJson();
+}
+
+async function hentJson() {
+	const response = await fetch(url);
+	side = await response.json();
+	vis();
+}
+
+function vis() {
+	document.querySelector("navn").textcontent = side.title.rendered;
+	document.querySelector("navn").innerHTML = side.content.rendered;
+}
