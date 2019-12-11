eadfa820e6c3c406efe6364f5a13f796050629c6
maja669e <maja669e@stud.kea.dk>
2019-12-11 10:48:46 +0100

    første udkast done

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ const url = "https://katjsoeg.dk/kea/2_sem_eksamen/designwp/wordpress/wp-json/wp
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-	hentJson();
+    hentJson();
+
 }
 
 async function hentJson() {
-	const response = await fetch(url);
-	side = await response.json();
-	vis();
+    const response = await fetch(url);
+    side = await response.json();
+    vis();
 }
 
 function vis() {
-	document.querySelector("navn").textcontent = side.title.rendered;
-	document.querySelector("navn").innerHTML = side.content.rendered;
+    document.querySelector("navn").textcontent = side.title.rendered;
+    document.querySelector("navn").innerHTML = side.content.rendered;
+}
+
+
+function burgerMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
